5c8ffce4a9d9c4da86fa5d46f2f44c54e379d06b
rudy-p <rudewire@gmail.com>
2019-09-14 09:58:28 -0700

    First commit with simple RMS program

---
+++ test-file.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdint.h>
+
+#define ARRAY_SIZE_MAX 3
+
+double
+rms(double* array, size_t n)
+{
+	double sum = 0.0;
+
+	for(int i = 0; i < ARRAY_SIZE_MAX; i++)
+	{
+		sum += array[i] * array[i];		
+	}
+
+	return(sqrtf(sum/n));
+}
+
+void
+main (void)
+{
+	double arr[ARRAY_SIZE_MAX] = {1.0, 2.0, 3.0};
+
+	printf("\nThe rms of array is: %0.8f\n\n", rms(arr, ARRAY_SIZE_MAX));
+}
